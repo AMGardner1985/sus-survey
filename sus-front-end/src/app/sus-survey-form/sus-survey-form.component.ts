bb06fa41b9093c724360baf3a2e49cf13665d06a
Adam Gardner <AdamGardner33@gmail.com>
2020-02-23 10:47:17 -0500

    added in some validation then simplified validation on submit

--- sus-front-end/src/app/sus-survey-form/sus-survey-form.component.ts
+++ sus-front-end/src/app/sus-survey-form/sus-survey-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SusSurvey } from './data/sus-survey';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sus-survey-form',
@@ -9,7 +11,29 @@ export class SusSurveyFormComponent implements OnInit {
 
   constructor() { }
 
+  originalSusSurvey : SusSurvey = {
+    respondant: null,
+    application: null,
+    version: null,
+    q1: null,
+    q2: null,
+    q3: null,
+    q4: null,
+    q5: null,
+    q6: null,
+    q7: null,
+    q8: null,
+    q9: null,
+    q10: null
+  };
+
+  susSurvey : SusSurvey = { ...this.originalSusSurvey };
+
   ngOnInit() {
+  }
+
+  onSubmit(form : NgForm){
+    console.log('in onSubmit: ', form.valid);
   }
 
 }
